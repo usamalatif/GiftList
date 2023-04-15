8118ac468dd80b28e5fb79853c53b682a84cdcdf
Usama Latif <usamalatif25@gmail.com>
2023-04-15 05:31:54 +0500

    MERKLE Tree Implementation

--- client/index.js
+++ client/index.js
@@ -3,12 +3,19 @@ const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
+const name = 'Usama Latif';
+const merkleTree = new MerkleTree(niceList);
+
+const index = niceList.findIndex(n => n === name);
+const proof = merkleTree.getProof(index);
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name:name,
+    proof:proof
   });
 
   console.log({ gift });
